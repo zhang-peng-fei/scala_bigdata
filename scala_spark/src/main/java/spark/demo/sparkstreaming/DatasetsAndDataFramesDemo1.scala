fca7fc770274565a65fb5423d6a35bf3ce16dced
zhang-peng-fei <1097895295@qq.com>
2020-04-27 18:17:59 +0800

    Spark Sql官网样例Demo，DataSet和DataFrame

--- scala_spark/src/main/java/spark/demo/sparkstreaming/DatasetsAndDataFramesDemo1.scala
+++ scala_spark/src/main/java/spark/demo/sparkstreaming/DatasetsAndDataFramesDemo1.scala
@@ -1,8 +1,11 @@
 package spark.demo.sparkstreaming
 
+import java.util.Properties
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import spark.SparkHiveExample.Record
 import spark.utils.CommUtil
 
 object DatasetsAndDataFramesDemo1 {
@@ -16,10 +19,11 @@ object DatasetsAndDataFramesDemo1 {
       .builder()
       .appName("Spark SQL basic example")
       .master("local[2]")
-      //      .config("spark.some.config.option", "some-value")
-
+      .config("spark.sql.warehouse.dir", "warehouseLocation")
+      .enableHiveSupport()
       .getOrCreate()
-    Logger.getRootLogger.setLevel(Level.WARN)
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    //    Logger.getRootLogger.setLevel(Level.WARN)
 
     // For implicit conversions like converting RDDs to DataFrames(用于隐式转换，如将RDDs转换为DataFrames)
     import spark.implicits._
@@ -130,27 +134,135 @@ object DatasetsAndDataFramesDemo1 {
     results.map(attributes => "Name: " + attributes(0)).show()
     val userParquetPath = CommUtil.getResourcePath + "users.parquet"
     val usersDF = spark.read.load(userParquetPath)
-    usersDF.select("name", "favorite_color").write.save(CommUtil.getResourcePath + "namesAndFavColors.parquet")
+    usersDF.select("name", "favorite_color").write.mode("ignore").save(CommUtil.getResourcePath + "namesAndFavColors.parquet")
 
     val peopleDF3 = spark.read.format("json").load(peopleJsonPath)
-    peopleDF3.select("name", "age").write.format("parquet").save("namesAndAges.parquet")
+    peopleDF3.select("name", "age").write.mode("ignore").format("parquet").save(CommUtil.dataPath + "namesAndAges.parquet")
     // 也可以使用 SQL 直接查询该文件
-    val sqlDF2 = spark.sql("SELECT * FROM parquet.'" + userParquetPath + "'")
-    sqlDF2.write.mode("overwrite").saveAsTable("sqlDF2")
+    val sqlDF2 = spark.sql("SELECT * FROM parquet.`" + userParquetPath + "`")
+    sqlDF2.write.mode("ignore").saveAsTable("sqlDF2")
     // Parquet Files--Loading Data Programmatically
     val peopleDF4 = spark.read.json(peopleJsonPath)
 
     // DataFrames can be saved as Parquet files, maintaining the schema information
-    peopleDF4.write.parquet("people.parquet")
+    peopleDF4.select("*").show()
+    peopleDF4.write.mode("ignore").parquet(CommUtil.dataPath + "parquet/people.parquet")
 
     // Read in the parquet file created above
     // Parquet files are self-describing so the schema is preserved
     // The result of loading a Parquet file is also a DataFrame
-    val parquetFileDF = spark.read.parquet("people.parquet")
+    val parquetFileDF = spark.read.schema(schema).parquet(CommUtil.dataPath + "parquet/people.parquet/")
 
     // Parquet files can also be used to create a temporary view and then used in SQL statements
     parquetFileDF.createOrReplaceTempView("parquetFile")
     val namesDF = spark.sql("SELECT name FROM parquetFile WHERE age BETWEEN 13 AND 19")
     namesDF.map(attributes => "Name: " + attributes(0)).show()
+
+    // Create a simple DataFrame, store into a partition directory
+    val squaresDF = spark.sparkContext.makeRDD(1 to 5).map(i => (i, i * i)).toDF("value", "square")
+    squaresDF.write.mode("ignore").parquet(CommUtil.dataPath + "test_table/key=1/")
+
+    // Create another DataFrame in a new partition directory,
+    // adding a new column and dropping an existing column
+    val cubesDF = spark.sparkContext.makeRDD(6 to 10).map(i => (i, i * i * i)).toDF("value", "cube")
+    cubesDF.write.mode("ignore").parquet(CommUtil.dataPath + "test_table/key=2/")
+
+    // Read the partitioned table
+    val mergedDF = spark.read.option("mergeSchema", "true").schema(schema).parquet(CommUtil.dataPath + "test_table")
+    mergedDF.printSchema()
+    mergedDF.show()
+
+    // spark is an existing SparkSession
+    spark.catalog.refreshTable("my_table")
+
+    val peopleDF5 = spark.read.json(peopleJsonPath)
+
+    // The inferred schema can be visualized using the printSchema() method
+    peopleDF5.printSchema()
+
+    // Creates a temporary view using the DataFrame
+    peopleDF5.createOrReplaceTempView("people")
+
+    // SQL statements can be run by using the sql methods provided by spark
+    val teenagerNamesDF = spark.sql("SELECT name FROM people WHERE age BETWEEN 13 AND 19")
+    teenagerNamesDF.show()
+
+    // Alternatively, a DataFrame can be created for a JSON dataset represented by
+    // an RDD[String] storing one JSON object per string
+    val otherPeopleRDD = spark.sparkContext.makeRDD(
+      """{"name":"Yin","address":{"city":"Columbus","state":"Ohio"}}""" :: Nil)
+    val otherPeople = spark.read.json(otherPeopleRDD)
+    otherPeople.show()
+
+    //    Hive Tables
+    import spark.sql
+    sql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING)")
+    sql("LOAD DATA LOCAL INPATH 'examples/src/main/resources/kv1.txt' INTO TABLE src")
+
+    // Queries are expressed in HiveQL
+    sql("SELECT * FROM src").show()
+
+    // Aggregation queries are also supported.
+    sql("SELECT COUNT(*) FROM src").show()
+
+    // The results of SQL queries are themselves DataFrames and support all normal functions.
+    val sqlDF3 = sql("SELECT key, value FROM src WHERE key < 10 ORDER BY key")
+
+    // The items in DaraFrames are of type Row, which allows you to access each column by ordinal.
+    val stringsDS = sqlDF3.map {
+      case Row(key: Int, value: String) => s"Key: $key, Value: $value"
+    }
+    stringsDS.show()
+
+    // You can also use DataFrames to create temporary views within a SparkSession.
+    val recordsDF = spark.createDataFrame((1 to 100).map(i => Record(i, s"val_$i")))
+    recordsDF.createOrReplaceTempView("records")
+
+    // Queries can then join DataFrame data with data stored in Hive.
+    sql("SELECT * FROM records r JOIN src s ON r.key = s.key").show()
+
+
+    // Note: JDBC loading and saving can be achieved via either the load/save or jdbc methods
+    // Loading data from a JDBC source
+    val jdbcDF = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:postgresql:dbserver")
+      .option("dbtable", "schema.tablename")
+      .option("user", "username")
+      .option("password", "password")
+      .load()
+
+    val connectionProperties = new Properties()
+    connectionProperties.put("user", "username")
+    connectionProperties.put("password", "password")
+    val jdbcDF2 = spark.read
+      .jdbc("jdbc:postgresql:dbserver", "schema.tablename", connectionProperties)
+
+    // Saving data to a JDBC source
+    jdbcDF.write
+      .format("jdbc")
+      .option("url", "jdbc:postgresql:dbserver")
+      .option("dbtable", "schema.tablename")
+      .option("user", "username")
+      .option("password", "password")
+      .save()
+
+    jdbcDF2.write
+      .jdbc("jdbc:postgresql:dbserver", "schema.tablename", connectionProperties)
+
+
+
+
+    // In 1.3.x, in order for the grouping column "department" to show up,
+    // it must be included explicitly as part of the agg function call.
+//    df.groupBy("department").agg($"department", max("age"), sum("expense"))
+
+    // In 1.4+, grouping column "department" is included automatically.
+//    df.groupBy("department").agg(max("age"), sum("expense"))
+
+    // Revert to 1.3 behavior (not retaining grouping column) by:
+//    sqlContext.setConf("spark.sql.retainGroupColumns", "false")
+
+
   }
 }
